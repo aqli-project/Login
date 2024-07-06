e3cbd30362a605df9ba724abd85273911111da7e
Aqli <aqli.aw05@gmail.com>
2024-07-06 08:28:42 +0700

    fix jam  di forex

--- app/src/main/java/com/example/login/ForexMainActivity.java
+++ app/src/main/java/com/example/login/ForexMainActivity.java
@@ -106,6 +106,9 @@ public class ForexMainActivity extends AppCompatActivity {
 
                 JSONObject rates;
 
+                long timestamp = System.currentTimeMillis() / 1000;
+                setTimestamp(timestamp);
+
                 try {
                     rates = root.getJSONObject("rates");
                 }catch (JSONException e){
